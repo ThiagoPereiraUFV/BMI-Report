4df4d62d6bcede19c26748cfbe9b358fe25c1ac8
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-10-07 10:01:11 -0300

    Main activity almost finished

--- app/src/main/java/com/example/bmireport/MainActivity.java
+++ app/src/main/java/com/example/bmireport/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.bmireport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,5 +49,57 @@ public class MainActivity extends AppCompatActivity {
 	protected void onDestroy() {
 		super.onDestroy();
 		Log.d("Ciclo", getLocalClassName() +  ": Activity destruída!");
+	}
+
+	private String getString(int id) {
+		return this.getResources().getString(id);
+	}
+
+	public void calculate(View v) {
+		try {
+			final String name = ((EditText) findViewById(R.id.editName)).getText().toString();
+			final int age = Integer.parseInt(((EditText) findViewById(R.id.editAge)).getText().toString());
+			final float weight = Float.parseFloat(((EditText) findViewById(R.id.editWeight)).getText().toString());
+			final float height = Float.parseFloat(((EditText) findViewById(R.id.editHeight)).getText().toString());
+
+			if(height == 0) {
+				Toast.makeText(this, this.getResources().getString(R.string.divByZero), Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			try {
+				final float bmi = weight/(height*height);
+				final String classif =
+						(bmi < 18.5) ?
+							getString(R.string.underWeight)
+						: (bmi < 25) ?
+							getString(R.string.healthy)
+						: (bmi < 30) ?
+							getString(R.string.overWeight)
+						: (bmi < 35) ?
+							getString(R.string.g1obesity)
+						: (bmi < 40) ?
+							getString((R.string.g2obesity))
+						: getString(R.string.g3obesity);
+
+				Bundle extras = new Bundle();
+				extras.putString("name", name);
+				extras.putInt("age", age);
+				extras.putFloat("weight", weight);
+				extras.putFloat("height", height);
+				extras.putFloat("bmi", bmi);
+				extras.putString("classif", classif);
+
+				final Intent it = new Intent(this, Report.class);
+				it.putExtras(extras);
+				startActivity(it);
+			} catch(Exception e) {
+				Toast.makeText(this, getString(R.string.resultError), Toast.LENGTH_SHORT).show();
+				Log.e("AppError", Objects.requireNonNull(e.getMessage()));
+			}
+		} catch(Exception e) {
+			Toast.makeText(this, this.getResources().getString(R.string.invalidValues), Toast.LENGTH_SHORT).show();
+			Log.e("AppError", Objects.requireNonNull(e.getMessage()));
+		}
 	}
 }
