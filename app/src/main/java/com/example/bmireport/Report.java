4e2fde6abab8cdcc0a876c14d454b70503c35c76
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-10-07 10:46:20 -0300

    Main activity finished

--- app/src/main/java/com/example/bmireport/Report.java
+++ app/src/main/java/com/example/bmireport/Report.java
@@ -2,8 +2,10 @@ package com.example.bmireport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class Report extends AppCompatActivity {
 
@@ -11,7 +13,25 @@ public class Report extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d("Ciclo", getLocalClassName() +  ": Activity iniciada!");
+
+		final Intent it = getIntent();
+		final Bundle extras = it.getExtras();
+
 		setContentView(R.layout.activity_report);
+
+		final TextView name = (TextView) findViewById(R.id.getName);
+		final TextView age = (TextView) findViewById(R.id.getAge);
+		final TextView weight = (TextView) findViewById(R.id.getWeight);
+		final TextView height = (TextView) findViewById(R.id.getHeight);
+		final TextView bmi = (TextView) findViewById(R.id.getIndex);
+		final TextView classif = (TextView) findViewById(R.id.getClassification);
+
+		name.setText(extras.getString("name"));
+		age.setText(Integer.toString(extras.getInt("age")));
+		weight.setText(Float.toString(extras.getFloat("weight")));
+		height.setText(Float.toString(extras.getFloat("height")));
+		bmi.setText(Float.toString(extras.getFloat("bmi")));
+		classif.setText(extras.getString("classif"));
 	}
 
 	protected void onStart() {
