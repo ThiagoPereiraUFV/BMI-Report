ade97ee33bab6e031f4355c90c56ce5c66639215
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-10-07 09:18:36 -0300

    Monitoring activities life cycle

--- app/src/main/java/com/example/bmireport/Report.java
+++ app/src/main/java/com/example/bmireport/Report.java
@@ -3,12 +3,44 @@ package com.example.bmireport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Report extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d("Ciclo", getLocalClassName() +  ": Activity iniciada!");
 		setContentView(R.layout.activity_report);
+	}
+
+	protected void onStart() {
+		super.onStart();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity iniciada!");
+	}
+
+	protected void onRestart() {
+		super.onRestart();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity reiniciada!");
+	}
+
+	protected void onResume() {
+		super.onResume();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity retomada!");
+	}
+
+	protected void onPause() {
+		super.onPause();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity pausada!");
+	}
+
+	protected void onStop() {
+		super.onStop();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity interrompida!");
+	}
+
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d("Ciclo", getLocalClassName() +  ": Activity destruída!");
 	}
 }
